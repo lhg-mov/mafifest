bf1ed1ae43be2ce98a399fd20b8bd8aa53e884dc
Lai Hong <88992332+lhg-mov@users.noreply.github.com>
2023-01-29 19:00:52 +0700

    update again

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/pages(.*)',
+          destination: '/block',
+          permanent: true,
+        },
+      ]
+    },
+  }
